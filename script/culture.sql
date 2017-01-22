30edb3cac8a997c1833a2fc858d949981c221ec2
awalker <awalker@DESKTOP-OF98LEH>
2017-01-22 18:59:25 +0800

    save

--- script/culture.sql
+++ script/culture.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主機: 127.0.0.1
--- 產生時間： 2017-01-21 16:45:27
+-- 產生時間： 2017-01-22 11:50:06
 -- 伺服器版本: 10.1.19-MariaDB
 -- PHP 版本： 5.6.28
 
@@ -127,6 +127,13 @@ CREATE TABLE `filename` (
   `l_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- 資料表的匯出資料 `filename`
+--
+
+INSERT INTO `filename` (`f_filename`, `f_filetype`, `l_id`) VALUES
+('run', '.jpg', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -186,7 +193,9 @@ INSERT INTO `profile` (`p_sex`, `p_age`, `c_id`, `username`) VALUES
 ('M', 58, 13, 'fsedfsf'),
 ('M', 20, 14, 'qqqq'),
 ('M', 45, 15, 'aaaaaa'),
-('M', 45, 16, 'aaaaaa');
+('M', 45, 16, 'aaaaaa'),
+('M', 15, 17, 'Willseed'),
+('M', 15, 18, 'Willseed');
 
 -- --------------------------------------------------------
 
@@ -216,7 +225,8 @@ INSERT INTO `users` (`username`, `password`, `email`, `enabled`) VALUES
 ('qqqq', '437599f1ea3514f8969f161a6606ce18', 'qqqqq@qqqq.qqqq', 1),
 ('ranma', 'e10adc3949ba59abbe56e057f20f883e', 'maokao25@gmail.com', 1),
 ('root', 'e10adc3949ba59abbe56e057f20f883e', 'mkyong@test.org.tw', 1),
-('test', '098f6bcd4621d373cade4e832627b4f6', 'test@test.test', 1);
+('test', '098f6bcd4621d373cade4e832627b4f6', 'test@test.test', 1),
+('Willseed', '202cb962ac59075b964b07152d234b70', '1@1', 1);
 
 -- --------------------------------------------------------
 
@@ -245,15 +255,8 @@ INSERT INTO `user_roles` (`user_role_id`, `username`, `role`) VALUES
 (13, 'fsedfsf', 'ROLE_USER'),
 (14, 'qqqq', 'ROLE_USER'),
 (1, 'root', 'ROLE_USER'),
-(12, 'test', 'ROLE_USER');
-
--- --------------------------------------------------------
-
---
--- 替換檢視表以便查看 `webtest`
---
-CREATE TABLE `webtest` (
-);
+(12, 'test', 'ROLE_USER'),
+(17, 'Willseed', 'ROLE_USER');
 
 -- --------------------------------------------------------
 
@@ -263,15 +266,6 @@ CREATE TABLE `webtest` (
 DROP TABLE IF EXISTS `details`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `details`  AS  select `l`.`l_id` AS `l_id`,`l`.`l_type` AS `l_type`,`l`.`l_name` AS `l_name`,`l`.`l_xaxis` AS `l_xaxis`,`l`.`l_yaxis` AS `l_yaxis`,`l`.`a_id` AS `a_id`,`f`.`f_filename` AS `f_filename`,`f`.`f_filetype` AS `f_filetype`,`v`.`v_vaule` AS `v_vaule` from ((`laction` `l` left join `class_vaule` `v` on((`v`.`l_id` = `l`.`l_id`))) left join `filename` `f` on((`f`.`l_id` = `l`.`l_id`))) ;
-
--- --------------------------------------------------------
-
---
--- 檢視表結構 `webtest`
---
-DROP TABLE IF EXISTS `webtest`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `webtest`  AS  select `cal`.`c_type` AS `c_type`,`ca`.`a_name` AS `a_name`,`cf`.`f_column` AS `f_column`,`cv`.`v_vaule` AS `v_vaule` from (((`class_ activity` `ca` left join `class_all` `cal` on((`ca`.`c_id` = `cal`.`c_id`))) left join `class_field` `cf` on((`cf`.`c_id` = `ca`.`c_id`))) left join `class_vaule` `cv` on((`cf`.`f_id` = `cv`.`f_id`))) ;
 
 --
 -- 已匯出資料表的索引
@@ -374,7 +368,7 @@ ALTER TABLE `laction`
 -- 使用資料表 AUTO_INCREMENT `user_roles`
 --
 ALTER TABLE `user_roles`
-  MODIFY `user_role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `user_role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- 已匯出資料表的限制(Constraint)
 --
